01ba1af3b9b450d718208604c30d07a89584f214
Nasim <nsm.amanii@gmail.com>
2023-03-11 23:33:48 +0330

    add BASD.m

---
+++ BASD.m
@@ -0,0 +1,28 @@
+clc;close all; clear;
+%simulation parameters
+SNR_dB_sim=0:1:11;
+SNR_dB=0:0.1:11;
+%Initialization
+smld_err_prb = zeros(1,length(SNR_dB_sim));
+theo_err_prb = zeros(1,length(SNR_dB));
+%% simulated error rate
+for i=1:length(SNR_dB_sim)
+    disp(SNR_dB_sim(i));
+    smld_err_prb(i)=BER_BASD(SNR_dB_sim(i));
+end
+%% theoretical error rate
+for i=1:length(SNR_dB)
+    SNR = exp(SNR_dB(i)*log(10)/10);
+    theo_err_prb(i)=qfunc(sqrt(2*SNR));
+end
+% SNR = exp(SNR_dB*log(10)/10);
+% theo_err_prb = qfunc(sqrt(2*SNR));
+%% Plotting commands follow.
+semilogy(SNR_dB_sim,smld_err_prb,'r*','LineWidth',1.5);
+hold
+semilogy(SNR_dB,theo_err_prb,'LineWidth',1.5);
+grid;
+axis([SNR_dB(1) SNR_dB(end) 1e-6 1e0]);
+xlabel('SNR_{dB}');
+ylabel('BER');
+title('Binary Antipodal Modulation in AWGN channel');
